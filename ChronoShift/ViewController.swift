a4f403f6e81658c7a732230c29bbc0fbf0e5dc6d
Zachary Smoak <zachary_smoak@comcast.com>
2018-11-05 21:52:23 -0500

    Initial Commit

---
+++ ChronoShift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChronoShift
+//
+//  Created by Smoak, Zachary on 11/5/18.
+//  Copyright © 2018 Smoak, Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
